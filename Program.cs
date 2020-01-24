a2c0560cdfc1762c0eec92972fbd23b720662206
amna1511 <amna.13aug@gmail.com>
2020-01-24 09:30:06 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] array = new int[6];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+              Console.WriteLine(" {0}",array[i] = random.Next(1,40));
+              
+            }
+            Console.ReadKey(); 
+            //for (int i = 1; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(" {0}", array[i]);
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
